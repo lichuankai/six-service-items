cc7b538e51f59bc6e1a54bb213cbab85392a85e2
Chunle Tian <chuntian997@gmail.com>
2017-11-02 14:03:44 +0800

    添加专利检索

--- ant-motion-dva-cli-example-2.0/src/routes/Home/Content1.jsx
+++ ant-motion-dva-cli-example-2.0/src/routes/Home/Content1.jsx
@@ -1,147 +1,85 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import TweenOne from 'rc-tween-one';
+import QueueAnim from 'rc-queue-anim';
 import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
-import { Tabs } from 'antd';
-
-const TabPane = Tabs.TabPane;
 
 class Content extends React.Component {
 
   static propTypes = {
-    id: PropTypes.string,
     className: PropTypes.string,
+    id: PropTypes.string,
   };
 
   static defaultProps = {
-    className: 'content6',
+    className: 'content5',
   };
 
-  state = {
-    show: 0,
-  };
+  getBlockChildren = (item, i) =>
+    (<li
+      key={i}
+      id={`${this.props.id}-block${i}`}
+    >
+      <span>
+        <img src={item.img} width="100%" />
+      </span>
+      <h2>{item.title}</h2>
+      <p>{item.content}</p>
+    </li>);
 
-  onChange = (key) => {
-    this.setState({ show: parseInt(key) })
-  }
-
-  getBlockChildren = (item, i) => {
-    const tag = item.tag;
-    const img = item.img;
-    const text = item.text;
-    return (
-      <TabPane
-        key={i}
-        tab={(<span
-          className={`${this.props.className}-tag`}
-          id={`${this.props.id}-tagBlock${i}`}
-        >
-          <i><img src={tag.icon} width="100%" /></i>
-          {tag.tag}
-        </span>)}
-      >
-        <TweenOne.TweenOneGroup
-          enter={{ y: 30, delay: 300, opacity: 0, type: 'from', ease: 'easeOutQuad' }}
-          leave={null}
-          component=""
-        >
-          {this.state.show === i && (
-            <div key="content">
-              <div
-                className={`${this.props.className}-img`}
-                id={`${this.props.id}-imgBlock${i}`}
-              >
-                {img}
-              </div>
-              <div
-                className={`${this.props.className}-text`}
-                id={`${this.props.id}-textBlock${i}`}
-                dangerouslySetInnerHTML={{ __html: text }}
-              />
-            </div>)}
-        </TweenOne.TweenOneGroup>
-      </TabPane>
-    );
-  };
 
   render() {
     const props = { ...this.props };
-    delete props.isMode;
-    const childrenData = [
-      {
-        tag: { tag: 'PHONE', icon: 'https://zos.alipayobjects.com/rmsportal/XnzcslQvRoBHMHd.svg' },
-        img: <img width="100%" src="https://zos.alipayobjects.com/rmsportal/xBrUaDROgtFBRRL.png" />,
-        text: `<h3>技术</h3>
-丰富的技术组件，简单组装即可快速搭建金融级应用，丰富的技术组件，简单组装即可快速搭建金融级应用。
-<h3>融合</h3>
-解放业务及技术生产力，推动金融服务底层创新，推动金融服务底层创新。解放业务及技术生产力，推动金融服务底层创新。
-<h3>开放</h3>
-符合金融及要求的安全可靠、高可用、高性能的服务能力，符合金融及要求的安全可靠、高可用、高性能的服务能力。`,
-      },
-      {
-        tag: { tag: 'TABLET', icon: 'https://zos.alipayobjects.com/rmsportal/XnzcslQvRoBHMHd.svg' },
-        img: <img width="100%" src="https://zos.alipayobjects.com/rmsportal/xBrUaDROgtFBRRL.png" />,
-        text: `<h3>技术</h3>
-丰富的技术组件，简单组装即可快速搭建金融级应用，丰富的技术组件，简单组装即可快速搭建金融级应用。
-<h3>融合</h3>
-解放业务及技术生产力，推动金融服务底层创新，推动金融服务底层创新。解放业务及技术生产力，推动金融服务底层创新。
-<h3>开放</h3>
-符合金融及要求的安全可靠、高可用、高性能的服务能力，符合金融及要求的安全可靠、高可用、高性能的服务能力。`,
-      },
-      {
-        tag: { tag: 'DESKTOP', icon: 'https://zos.alipayobjects.com/rmsportal/XnzcslQvRoBHMHd.svg' },
-        img: <img width="100%" src="https://zos.alipayobjects.com/rmsportal/xBrUaDROgtFBRRL.png" />,
-        text: `<h3>技术</h3>
-丰富的技术组件，简单组装即可快速搭建金融级应用，丰富的技术组件，简单组装即可快速搭建金融级应用。
-<h3>融合</h3>
-解放业务及技术生产力，推动金融服务底层创新，推动金融服务底层创新。解放业务及技术生产力，推动金融服务底层创新。
-<h3>开放</h3>
-符合金融及要求的安全可靠、高可用、高性能的服务能力，符合金融及要求的安全可靠、高可用、高性能的服务能力。`,
-      },
+    const isMode = props.isMode;
+    const dataSource = [
+      { img: '', title: '委托方式', content: '当面受理、网上受理、传真受理、委托受理等。' },
+      { img: '', title: '成果鉴定', content: '为科技工作者、大专院校、企业及个人提供结题的查新检索。' },
+      { img: '', title: '科技（专利）奖项评审检索', content: ' 根据《专利奖项评价指标》及《科技奖项评价指标》，针对奖项的具体要求出具检索分析报告。' },
+      { img: '', title: '科技计划，立项查新', content: ' 国家863计划、973计划、科技攻关计划、软科学计划、火炬计划、国际合作计划、重点新产品计划、中小企业创新基金等各级政府部门立项项目。' },
     ];
-    const tabsChildren = childrenData.map(this.getBlockChildren);
+    const ulChildren = dataSource.map(this.getBlockChildren);
+    delete props.isMode;
+    const queue = isMode ? 'bottom' : 'left';
+    const imgAnim = isMode ? { y: 30, opacity: 0, delay: 400, type: 'from', ease: 'easeOutQuad' }
+      : { x: 30, opacity: 0, type: 'from', ease: 'easeOutQuad' };
     return (
-      <div
-        {...props}
-        className={`content-template-wrapper ${props.className}-wrapper`}
-      >
+      <div {...props} className="content-template-wrapper content5-wrapper">
         <OverPack
           className={`content-template ${props.className}`}
           location={props.id}
         >
+          <QueueAnim
+            className={`${props.className}-text`}
+            key="text"
+            type={queue}
+            leaveReverse
+            ease={['easeOutQuad', 'easeInQuad']}
+            id={`${props.id}-textWrapper`}
+          >
+           
+            <QueueAnim
+              component="ul"
+              key="ul" type={queue}
+              id={`${props.id}-ul`}
+              ease="easeOutQuad"
+            >
+              {ulChildren}
+            </QueueAnim>
+          </QueueAnim>
           <TweenOne
-            animation={{ y: '+=30', opacity: 0, type: 'from' }}
-            component="h1"
-            key="h1"
-            reverseDelay={200}
-            id={`${props.id}-title`}
+            className={`${props.className}-img`}
+            key="img"
+            animation={imgAnim}
+            id={`${props.id}-img`}
+            resetStyleBool
           >
-            蚂蚁金融云提供专业的服务
+            <img width="100%" src="retrieval.png" />
           </TweenOne>
-          <TweenOne
-            animation={{ y: '+=30', opacity: 0, type: 'from', delay: 100 }}
-            component="p"
-            key="p"
-            reverseDelay={100}
-            id={`${props.id}-content`}
-          >
-            科技想象力，金融创造力
-          </TweenOne>
-          <TweenOne.TweenOneGroup
-            key="tabs"
-            enter={{ y: 30, opacity: 0, delay: 200, type: 'from' }}
-            leave={{ y: 30, opacity: 0 }}
-            className={`${props.className}-tabs`}
-            id={`${props.id}-tabs`}
-          >
-            <Tabs key="tabs" onChange={this.onChange} activeKey={`${this.state.show}`}>
-              {tabsChildren}
-            </Tabs>
-          </TweenOne.TweenOneGroup>
         </OverPack>
       </div>
     );
   }
 }
+
 
 export default Content;
